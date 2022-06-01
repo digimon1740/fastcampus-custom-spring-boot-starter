55447441856ffd62975c69dc5b5a099b0ccacc23
sanghoon <digimon.1740@gmail.com>
2022-06-01 23:48:50 +0900

    코드 수정

--- handgame-spring-boot-autoconfigure/src/main/kotlin/com/fastcampus/springboot/autoconfigure/handgame/HandgameAutoconfiguration.kt
+++ handgame-spring-boot-autoconfigure/src/main/kotlin/com/fastcampus/springboot/autoconfigure/handgame/HandgameAutoconfiguration.kt
@@ -1,13 +1,13 @@
 package com.fastcampus.springboot.autoconfigure.handgame
 
 import com.fastcampus.springboot.handgame.Handgame
+import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 
-@Configuration
+@AutoConfiguration
 @ConditionalOnClass(Handgame::class)
 @ConditionalOnProperty(prefix = "my.handgame", name = ["enabled"], havingValue = "true")
 class HandgameAutoconfiguration {
